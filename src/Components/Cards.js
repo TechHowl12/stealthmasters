15f9b15445349e7c47443948f7cd51ec11af6959
codewithafzall <afzalshaikh19992@gmail.com>
2024-05-14 11:55:13 +0530

    completed

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,78 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SmallWhite from "../assets/SmallWhite.png";
+import SmallBrown from "../assets/SmallBrown.png";
+import SmallBlack from "../assets/SmallBlack.png";
+import MediumWhite from "../assets/MediumWhite.png"
 import MediumBrown from "../assets/MediumBrown.png"
-import LargeBlack from "../assets/BravoLarge.png"
-import YankeeBrown from "../assets/YankeeLarge.png"
+import MediumBlack from "../assets/MediumBlack.png"
+import LargeWhite from "../assets/LargeWhite.png"
+import LargeBlack from "../assets/LargeBlack.png"
+import LargeBrown from "../assets/LargeBrown.png"
+import YankeeWhite from "../assets/YankeeLarge.png"
 
 export const Cards = () => {
+
+  const [smallCase,setSmallCase] = useState(SmallWhite);
+  const [mediumCase,setMediumCase] = useState(MediumBrown);
+  const [largeCase,setLargeCase] = useState(LargeBlack);
+
   return (
-    <div className='bg-white p-14'>
+    <div className='bg-white p-8 md:p-14'>
         <div className='bg-[#E6DCC8] rounded-3xl p-7'>
-            <h3 className='text-[#807316] font-semibold tracking-wide mt-[11%] text-2xl text-center italic'>Explore Our Range of Colors and Sizes</h3>
+            <h3 className='text-[#823205] font-semibold tracking-wide mt-7 md:mt-[9%] lg:mt-[11%] text-2xl text-center'>Explore Our Range of Colors and Sizes</h3>
 
             <div className='grid grid-cols-2 gap-7 mt-10'>
 
-                 <div className='border border-white rounded-xl sm:p-4 p-7 h-[32rem] sm:h-[31rem]'>
-                    <div className='h-2/3 flex items-center'>
-                      <img className='w-5/6 mx-auto' src={SmallWhite} alt='photo of a small white gun case'/>
+            <div className='border sm:p-4 border-white rounded-xl p-7 sm:h-[28rem] h-[25rem]'>
+            <div className='h-2/3 relative flex items-center justify-center'>
+                      <img className='w-5/6 absolute top-5' src={smallCase} alt='small gun case'/>
                     </div>
-                    <h5 className='italic font-semibold sm:mt-3 mt-8'>Bravo Small MSRP<span className='font-normal font-sans not-italic pl-4 text-sm'>(Fits One Handgun)</span></h5>
-                     <div className='flex gap-x-2 mt-3'>
-                        <button className='w-4 h-4 bg-black rounded-sm'></button>
-                        <button className='w-4 h-4 bg-[#926641] rounded-sm'></button>
-                        <button className='w-4 h-4 bg-white rounded-sm'></button>
+                    <h5 className='font-semibold sm:mt-3 mt-8'>Bravo Small MSRP<span className='inline sm:block font-normal roboto pl-4 sm:pl-0 text-sm'>(Fits One Handgun)</span></h5>
+                     <div className='flex gap-x-2 mt-1'>
+                        <button onClick={()=>setSmallCase(SmallBlack)} className='w-4 h-4 bg-black rounded-sm cursor-pointer'></button>
+                        <button onClick={()=>setSmallCase(SmallBrown)} className='w-4 h-4 bg-[#926641] rounded-sm cursor-pointer'></button>
+                        <button onClick={()=>setSmallCase(SmallWhite)} className='w-4 h-4 bg-white block z-10 rounded-sm cursor-pointer'></button>
                      </div>
                      <div className='border mb-4 border-white p-1 mt-4 rounded-md flex justify-between items-center'>
-                         <h5 className='text-[#807316] font-sans text-xl font-bold'>$199</h5>
-                         <button className='bg-[#807316] text-white font-semibold tracking-wide italic p-2 rounded-md'>Buy Now</button>
+                         <h5 className='text-[#823205] roboto text-xl font-bold'>$199</h5>
+                         <button className='border border-[#807316] text-[#807316] tracking-wide font-medium py-1 px-3 rounded-md hover:bg-[#807316] hover:text-white transition-all'>Buy Now</button>
+                     </div>
+                 </div>
+
+                 <div className='border sm:p-4 border-white rounded-xl p-7 sm:h-[28rem] h-[25rem]'>
+                    <div className='h-2/3 relative flex items-center justify-center'>
+                      <img className='w-5/6 absolute top-5' src={mediumCase} alt='medium gun case'/>
+                    </div>
+                    <h5 className='font-semibold sm:mt-3 mt-8'>Bravo Medium MSRP<span className='inline sm:block font-normal roboto pl-4 sm:pl-0 text-sm'>(Fits Two Handguns)</span></h5>
+                     <div className='flex gap-x-2 mt-1'>
+                        <button onClick={()=>setMediumCase(MediumBlack)} className='w-4 h-4 bg-black rounded-sm'></button>
+                        <button onClick={()=>setMediumCase(MediumBrown)} className='w-4 h-4 bg-[#926641] rounded-sm'></button>
+                        <button onClick={()=>setMediumCase(MediumWhite)} className='w-4 h-4 bg-white block z-10 rounded-sm'></button>
+                     </div>
+                     <div className='border mb-4 pl-3 border-white p-1 mt-4 rounded-md flex justify-between items-center'>
+                         <h5 className='text-[#823205] roboto text-xl font-bold'>$279</h5>
+                         <button className='border border-[#807316] text-[#807316] tracking-wide font-medium py-1 px-3 rounded-md hover:bg-[#807316] hover:text-white transition-all'>Buy Now</button>
+                     </div>
+                 </div>
+
+                 <div className='border sm:p-4 border-white rounded-xl p-7 sm:h-[28rem] h-[25rem]'>
+                 <div className='h-2/3 relative flex items-center justify-center'>
+                      <img className='w-5/6 absolute top-5' src={largeCase} alt='large gun case'/>
+                    </div>
+                    <h5 className='font-semibold sm:mt-3 mt-8'>Bravo Large MSRP<span className='inline sm:block font-normal roboto pl-4 sm:pl-0 text-sm'>(Fits Two Handguns & One Assault Rifle)</span></h5>
+                     <div className='flex gap-x-2 mt-1'>
+                        <button onClick={()=>setLargeCase(LargeBlack)} className='w-4 h-4 bg-black rounded-sm'></button>
+                        <button onClick={()=>setLargeCase(LargeBrown)} className='w-4 h-4 bg-[#926641] rounded-sm'></button>
+                        <button onClick={()=>setLargeCase(LargeWhite)} className='w-4 h-4 bg-white block z-10 rounded-sm'></button>
+                     </div>
+                     <div className='border mb-4 pl-3 border-white p-1 mt-4 rounded-md flex justify-between items-center'>
+                         <h5 className='text-[#823205] roboto text-xl font-bold'>$379</h5>
+                         <button className='border border-[#807316] text-[#807316] tracking-wide font-medium py-1 px-3 rounded-md hover:bg-[#807316] hover:text-white transition-all'>Buy Now</button>
                      </div>
                  </div>
 
                  <div className='border sm:p-4 border-white rounded-xl p-7'>
-                    <div className='h-2/3 flex items-center'>
-                        <img className='w-5/6 mx-auto' src={MediumBrown} alt='photo of a small white gun case'/>
+                    <div className='h-2/3 flex justify-center items-center'>
+                      <img className='w-5/6 mx-auto' src={YankeeWhite} alt='photo of a small white gun case'/>
                     </div>
-                    <h5 className='italic font-semibold sm:mt-3 mt-8'>Bravo Medium MSRP<span className='font-normal font-sans not-italic pl-4 text-sm'>(Fits Two Handguns)</span></h5>
-                     <div className='flex gap-x-2 mt-3'>
-                        <button className='w-4 h-4 bg-black rounded-sm'></button>
-                        <button className='w-4 h-4 bg-[#926641] rounded-sm'></button>
+                    <h5 className='font-semibold sm:mt-3 mt-8'>Bravo Small MSRP<span className='inline sm:block font-normal roboto pl-4 sm:pl-0 text-sm'>(Fits One Handgun & One Assault Rifle)</span></h5>
+                     <div className='flex gap-x-2 mt-1'>
                         <button className='w-4 h-4 bg-white rounded-sm'></button>
                      </div>
                      <div className='border mb-4 pl-3 border-white p-1 mt-4 rounded-md flex justify-between items-center'>
-                         <h5 className='text-[#807316] font-sans text-xl font-bold'>$279</h5>
-                         <button className='bg-[#807316] text-white font-semibold tracking-wide italic p-2 rounded-md'>Buy Now</button>
-                     </div>
-                 </div>
-
-                 <div className='border sm:p-4 border-white rounded-xl p-7 sm:h-[31rem] h-[32rem]'>
-                    <div className='h-2/3 flex items-center'>
-                      <img className='w-5/6 mx-auto' src={LargeBlack} alt='photo of a small white gun case'/>
-                    </div>
-                    <h5 className='italic font-semibold sm:mt-3 mt-8'>Bravo Large MSRP<span className='font-normal font-sans not-italic pl-4 text-sm'>(Fits Two Handguns & One Assault Rifle)</span></h5>
-                     <div className='flex gap-x-2 mt-3'>
-                        <button className='w-4 h-4 bg-black rounded-sm'></button>
-                        <button className='w-4 h-4 bg-[#926641] rounded-sm'></button>
-                        <button className='w-4 h-4 bg-white rounded-sm'></button>
-                     </div>
-                     <div className='border mb-4 pl-3 border-white p-1 mt-4 rounded-md flex justify-between items-center'>
-                         <h5 className='text-[#807316] font-sans text-xl font-bold'>$379</h5>
-                         <button className='bg-[#807316] text-white font-semibold tracking-wide italic p-2 rounded-md'>Buy Now</button>
-                     </div>
-                 </div>
-
-                 <div className='border sm:p-4 border-white rounded-xl p-7'>
-                    <div className='h-2/3 flex items-center'>
-                      <img className='w-5/6 mx-auto' src={YankeeBrown} alt='photo of a small white gun case'/>
-                    </div>
-                    <h5 className='italic font-semibold sm:mt-3 mt-8'>Bravo Small MSRP<span className='font-normal font-sans not-italic pl-4 text-sm'>(Fits One Handgun & One Assault Rifle)</span></h5>
-                     <div className='flex gap-x-2 mt-3'>
-                        <button className='w-4 h-4 bg-black rounded-sm'></button>
-                        <button className='w-4 h-4 bg-[#926641] rounded-sm'></button>
-                        <button className='w-4 h-4 bg-white rounded-sm'></button>
-                     </div>
-                     <div className='border mb-4 pl-3 border-white p-1 mt-4 rounded-md flex justify-between items-center'>
-                         <h5 className='text-[#807316] font-sans text-xl font-bold'>$349</h5>
-                         <button className='bg-[#807316] text-white font-semibold tracking-wide italic p-2 rounded-md'>Buy Now</button>
+                         <h5 className='text-[#823205] roboto text-xl font-bold'>$349</h5>
+                         <button className='border border-[#807316] text-[#807316] tracking-wide font-medium py-1 px-3 rounded-md hover:bg-[#807316] hover:text-white transition-all'>Buy Now</button>
                      </div>
                  </div>
             </div>
--- src/Components/Purpose.js
+++ src/Components/Purpose.js
@@ -3,28 +3,28 @@ import Design from "../assets/Design.png";
 
 export const Purpose = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 p-5 sm:mb-10 md:p-20">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 mt-7 sm:mt-0 md:mt-0 p-5 sm:mb-10 md:p-10">
       <div className="flex justify-center items-center">
         <img
-          className="w-4/6 sm:w-1/2 md:w-1/2"
+          className="w-1/3"
           src={Design}
-          alt="photo of security lock"
+          alt="security lock"
         />
       </div>
       <div>
-        <h1 className="text-[#807316] mt-7 md:mt-0 italic font-semibold text-4xl md:text-6xl">
+        <h1 className="text-[#823205] mt-5 md:mt-0 font-medium text-4xl md:text-6xl">
           Our Purpose
         </h1>
-        <p className="font-sans sm:text-md text-lg mt-7 md:mt-10">
-          <strong className="italic">Stealth-Masters</strong> is the stylish choice for <strong>discreet, secure, and easily
-          accessible storage</strong>. Our <strong>customizable concealment furniture</strong> securely
+        <p className="roboto sm:text-md text-lg mt-7">
+          <strong>Stealth-Masters</strong> is the stylish choice for <strong>discreet, secure, and easily
+          accessible storage</strong>.<br/><br/> Our <strong>customizable concealment furniture</strong> securely
           stores firearms or other personal items such as jewelry, recreational
           or prescription drugs, adult toys, and much more, blending seamlessly
-          with your home décor. Featuring <strong>electronic RFID locks</strong>, our innovative
+          with your home décor.<br/><br/> Featuring <strong>electronic RFID locks</strong>, our innovative
           products keep items easily accessible to you while <strong>remaining hidden
           from view.</strong>
         </p>
-        <p className="mt-4 md:mt-7 text-lg font-sans">For bulk orders email us at <a className="text-text-decoration-none text-[#807316] italic font-semibold" href="mailto:sales@stealth-masters.com">sales@stealth-masters.com</a></p>
+        <p className="mt-4 md:mt-7 text-lg font-sans">For bulk orders email us at <a className="text-text-decoration-none text-[#823205] italic font-semibold" href="mailto:sales@stealth-masters.com">sales@stealth-masters.com</a></p>
       </div>
     </div>
   );
